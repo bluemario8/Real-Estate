0118b9c9c28d05a6b4f353026c3217e7dfb3f014
bluemario8 <bluemario8@yahoo.com>
2024-11-01 15:44:59 -0400

    Made the header change color and size on scroll

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 $(function () {
+    let header = $('#header');
+    let logo = $('.logo')
+    let navBar = $('.nav-bar');
+
     console.log("Hello");
 
     $('#properties-slider').slick({
@@ -25,4 +29,37 @@ $(function () {
         prevArrow: '<a href="#" class="slick-arrow slick-prev"><</a>',
         nextArrow: '<a href="#" class="slick-arrow slick-next">></a>'
     });
-})
+
+    $(document).scroll(() => {
+        let scrollT = $(document).scrollTop();
+        if (scrollT > 0) {
+            header.css('background-color', '#fff');
+            logo.css('color', '#000');
+            navBar.addClass('black-orange');
+        }
+        else {
+            header.css('background-color', '');
+            logo.css('color', '');
+            navBar.removeClass('black-orange');
+        }
+
+        if (scrollT < 200) {
+            header.css({'padding': '1.5rem', 'box-shadow': 'none'});
+        }
+        else {
+            header.css({'padding': '0.9rem', 'box-shadow': '0 -4px 10px 1px'});
+        }
+    })
+
+    // document.addEventListener("scroll", (event) => {
+    //     let scrollTop = document.scrollTop;
+    //     console.log(scrollTop);
+    //     console.log("scrolling");
+
+    //     if (scrollTop > 0) {
+    //         console.log("Scrolled down");
+    //         // $('#header').css('background', '#fff');
+    //         console.log("hi");
+    //     }
+    // });
+});
